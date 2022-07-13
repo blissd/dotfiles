3debbae7fbbfe161a5e32cb0ee2792b2e6734cfe
David Bliss <github@davidbliss.name>
2022-07-13 14:07:17 +0100

    Work whether curl or wget is present

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-
-#sh -c "$(curl -fsSL https://git.io/zinit-install)"
-sh -c "$(wget -q -O - https://git.io/zinit-install)"
+if command -v curl &> /dev/null; then
+	sh -c "$(curl -fsSL https://git.io/zinit-install)"
+elif command -v wget &> /dev/null; then
+	sh -c "$(wget -q -O - https://git.io/zinit-install)"
+fi
 
