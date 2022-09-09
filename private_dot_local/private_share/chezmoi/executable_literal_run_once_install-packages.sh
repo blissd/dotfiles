7e47e295df64a9aabe627c03501d984ad72286b9
David Bliss <github@davidbliss.name>
2022-09-09 10:40:33 +0100

    Remove accidentally added file

--- private_dot_local/private_share/chezmoi/executable_literal_run_once_install-packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if command -v curl &> /dev/null; then
-	sh -c "$(curl -fsSL https://git.io/zinit-install)"
-elif command -v wget &> /dev/null; then
-	sh -c "$(wget -q -O - https://git.io/zinit-install)"
-fi
-
