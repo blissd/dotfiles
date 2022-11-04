c0df6b08b5973f4e012844a92391b29493cb0a3f
David Bliss <github@davidbliss.name>
2022-11-04 15:00:16 +0000

    Install vim-plug

--- run_once_1-install-packages.sh
+++ run_once_1-install-packages.sh
@@ -2,8 +2,15 @@
 
 # Install the 'pure' theme.
 if command -v git &> /dev/null; then
-	mkdir -p "$HOME/.zsh"
-	rm -rf $HOME/.zsh/pure
-	git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+	DIR="$HOME/.zsh/pure"
+	rm -rf $DIR
+	mkdir -p $DIR
+	git clone https://github.com/sindresorhus/pure.git $DIR
 fi
 
+# Install vim-plug for managing neovim plugins
+if command -v curl &> /dev/null; then
+	DIR="$HOME/.config/nvim/autoload"
+	mkdir -p $DIR
+	curl https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -o $DIR/plug.vim
+fi
